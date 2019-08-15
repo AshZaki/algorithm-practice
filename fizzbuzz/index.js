e05e02727624e3a40b6434fd0d6887803d145337
Ash Zaki <ashleya.zaki@gmail.com>
2019-08-15 12:11:25 -0400

    fixed it

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -16,13 +16,14 @@ function fizzBuzz(n) {
     for(let i = 1; i <= n; i++){
         //is the number a multiple of 3 and 5 ?
         if (i % 3 === 0 && i % 5 === 0){
-            console.log('fizzBuzz')
+            console.log('fizzbuzz')
         }
         else if (i % 3 === 0){
+            // Is number a multiple of 3?
             console.log('fizz')
         }
         else if (i % 5 === 0){
-            console.log('Buzz')
+            console.log('buzz')
         }
         else{
             console.log(i)
